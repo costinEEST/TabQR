b57f7aa6c731bbeb532ca5a77bc40a9ef938a12b
Constantin Câmpean <mail.la.costi@gmail.com>
2024-10-16 19:13:17 +0300

    build the extension for Firefox or Chrome

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # CLI 
 
-- `pnpm create vite-plugin-web-extension`
+- `pnpm build:<browserName>`
+
+# Docs 
+
+- https://vite-plugin-web-extension.aklinker1.io/guide/supporting-multiple-browsers.html#dynamic-manifests
+
+# Testing 
+
+- In Firefox:
+  - Open Firefox and go to `about:debugging#/runtime/this-firefox`.
+  - Click `Load Temporary Add-on...`.
+  - Select any file in the `dist` directory (like `manifest.json`), and Firefox will load your extension.
+- In Chrome:
+  - Open Chrome and go to `chrome://extensions` and enable `Developer mode`.
+  - Click `Load unpacked` and select the `dist` folder.
+  - Test the extension by clicking its icon in the toolbar.
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,32 @@
-import { defineConfig } from "vite";
+import { cwd } from "node:process";
+import { defineConfig, loadEnv, type UserConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 
-function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  };
-}
+export default defineConfig(({ mode }) => {
+  // https://vite.dev/config/#using-environment-variables-in-config
+  const env = loadEnv(mode, cwd(), "");
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    svelte(),
-    webExtension({
-      manifest: generateManifest,
-      watchFilePaths: ["package.json", "manifest.json"],
-    }),
-  ],
+  const config: UserConfig = {
+    plugins: [
+      svelte(),
+      webExtension({
+        browser: env.TARGET,
+        manifest: () => {
+          const manifest = readJsonFile("src/manifest.json");
+          const pkg = readJsonFile("package.json");
+
+          return {
+            name: pkg.name,
+            description: pkg.description,
+            version: pkg.version,
+            ...manifest,
+          };
+        },
+        watchFilePaths: ["package.json", "manifest.json"],
+      }),
+    ],
+  };
+
+  return config;
 });
